aab6d6e8cfaff1305d49cb42281a58c6b1340e14
liushengxi13689209566 <liushengxi13689209566@gmail.com>
2020-03-11 00:03:35 +0800

    SSM 框架整合成功

---
+++ HuaRunLingShou_03/src/main/java/com/huarun/dao/BookMapper.java
@@ -0,0 +1,24 @@
+package com.huarun.dao;
+
+import com.huarun.pojo.Books;
+
+import java.util.List;
+
+public interface BookMapper {
+    //增加一本书
+    int addBook(Books books);
+
+    //删除一本书
+    int deleteBookById(int id);
+
+    //更新一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(int id);
+
+    //查询全部的书
+    List<Books> queryAllBook();
+
+    Books queryBook(String queryBookName);
+}
